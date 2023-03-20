bc7487555574a7b5f8d3f13afbc65568f4bea4a8
Chris Anderson <cjanderson2@hotmail.com>
2023-03-20 21:51:33 +0000

    Update the UI tests to add tests on year logic.

---
+++ app/src/androidTest/java/com/chrisa/theoscars/util/AndroidComposeTestRuleExt.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 Chris Anderson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.chrisa.theoscars.util
+
+import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithStringId(
+    @StringRes id: Int,
+): SemanticsNodeInteraction = onNodeWithText(activity.getString(id))
